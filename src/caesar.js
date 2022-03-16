b2b6fd9869fc2432507b2c0c7b9211abfc493f05
Molly <mojonesycreative@gmail.com>
2022-03-16 13:32:04 -0400

    more work on caesar

--- src/caesar.js
+++ src/caesar.js
@@ -12,46 +12,58 @@ const caesarModule = (function () {
   function caesar(input, shift, encode = true) {
     try {
       if (shift === 0) throw Error("Shift value cannot be 0.");
-      if (shift < -25) throw Error("Shift value must be greater than -25.");
-      if (shift > 25) throw Error("Shift value must be less than 25.");
+      if (shift < -25 || shift > 25) throw Error("Shift value must be between -25 and 25.")
     } catch (error) {
       console.log(`${error}`);
       return false;
     }
 
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    let message = input.toLowerCase();
-    let inputArray = [];
-    for (let i = 0; i < message.length; i++) {
-      inputArray.push(message[i]);              //input string is now an array of elements//
-    }
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    encode = false ? shift *= -1 : shift *= 1;      //If set to decode, shift is set to a negative integer// 
+    let message = input
+      .toLowerCase()
+      .split('');
+    
     let result = [];
-    inputArray.forEach(([element]) => {
+    message.forEach(([element]) => {
       for (let i = 0; i < alphabet.length; i++) {
         const letter = alphabet[i];
+        if (!element === letter) result.push(element);
         if (element === letter) {                   //if message element matches letter in alphabet...
           const index = alphabet.indexOf(letter);
           let newLetter;
-          newLetter = shiftAlphabetToRight(shift)[index];      //newLetter is equal to the element at the given index of the shifted alphabet//
-          result.push(newLetter);                   
+            if (shift >= 1) {
+              newLetter = shiftAlphabetToLeft(shift)[index];      //newLetter is equal to the element at the given index of the shifted alphabet//
+              result.push(newLetter);
+            }
+            if (shift <= -1) {
+              newLetter = shiftAlphabetToRight(shift)[index];
+              result.push(newLetter);
+            }
         }
       }
     });
-    const output = result.join('');       //array is converted into a string//
-    return output;
+    return result.join('');
   }
 
 
 
-
-  function shiftAlphabetToRight(shiftNumber) {
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  function shiftAlphabetToLeft(shiftNumber) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     for (let i = 0; i < shiftNumber; i++) {
         alphabet.push(alphabet.shift());
     }
     return alphabet;
   }
 
+  function shiftAlphabetToRight(shiftNumber) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    shiftNumber *= -1;
+    for (let i = 0; i < shiftNumber; i++) {
+      alphabet.unshift(alphabet.pop());
+    }
+    return alphabet;
+  }
 
 
 
