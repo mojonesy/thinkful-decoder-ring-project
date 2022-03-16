14692bce5ca772d2c43be965c4ec2afcc64cc1c6
Molly <mojonesycreative@gmail.com>
2022-03-16 13:31:16 -0400

    Started polybius function and test

--- src/polybius.js
+++ src/polybius.js
@@ -6,13 +6,20 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
+
+
   function polybius(input, encode = true) {
-    // your solution code here
+    const alphabet = ['a','b','c','d','e','f','g','h','i/j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const polybiusSquare = [11,21,31,41,51,12,22,32,42,52,13,23,33,43,53,14,24,34,44,54,15,25,35,45,55];
+    let message = input.toLowerCase();
+    let result = '';
   }
 
   return {
     polybius,
   };
 })();
+
+
 
 module.exports = { polybius: polybiusModule.polybius };
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,9 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { polybius } = require("../src/polybius");
+
+describe("Polybius Test", () => {
+    it ("should be a function", () => {
+
+    });
+});
