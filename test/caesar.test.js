b02ac57b6ebfe9e36dec0deba3069cab1fd5520a
Molly <mojonesycreative@gmail.com>
2022-03-17 21:24:06 -0400

    Finished caesar tests

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -5,31 +5,41 @@ const { caesar } = require("../src/caesar");
 
 describe ("Caesar Test", () => {
     it ("should encode a message by shifting letters a set number of times", () => {
-        const actual = caesar("This is a secret message!", 8);
+        const actual = caesar('This is a secret message!', 8);
         const expected = "bpqa qa i amkzmb umaaiom!";
         expect(actual).to.equal(expected);
     });
-/*
-    it ("should decode a message by shifting letters a set number of times", () => {
 
+    it ("should decode a message by shifting letters a set number of times", () => {
+        const actual = 'thinkful';
+        const expected = caesar('wklqnixo', 3, false);
+        expect(actual).to.equal(expected);
     });
 
     it ("should maintain spaces and nonalphabetic symbols within shifting", () => {
-
+        const actual = 'bpqa qa i amkzmb umaaiom!';
+        const expected = caesar("This is a secret message!", 8);
+        expect(actual).to.equal(expected);
     });
 
     it ("should ignore capital letters", () => {
-
+        const actual = 'this is a secret message!';
+        const expected = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
+        expect(actual).to.equal(expected);
     });
 
     it ("should shift letters towards the end of the alphabet to the beginning if necessary", () => {
-
+        const actual = 'xqvvwnz';
+        const expected = caesar('Buzzard', -4);
+        expect(actual).to.equal(expected);
     });
 
     it ("should allow a negative integer to shift letters to the left", () => {
-
+        const actual = 'qefkhcri';
+        const expected = caesar('thinkful', -3);
+        expect(actual).to.equal(expected);
     });
-*/
+
 });
 
 
